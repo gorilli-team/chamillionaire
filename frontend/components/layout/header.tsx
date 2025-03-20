ed9c2de0f99013d5691eae53085b00455db2098c
Paolo Antonio Rossi <paolo@luduvigo.com>
2025-03-20 23:42:09 +0100

    frontend: improve header file

--- frontend/components/layout/header.tsx
+++ frontend/components/layout/header.tsx
@@ -5,6 +5,8 @@ import { useLogin, usePrivy, useWallets } from "@privy-io/react-auth";
 import { Button } from "../ui/button";
 import { cn } from "../../lib/utils";
 
+const BASE_CHAIN_ID = "eip155:8453";
+
 interface HeaderProps {
   className?: string;
 }
@@ -13,6 +15,8 @@ export function Header({ className }: HeaderProps) {
   const { ready, authenticated, user, login, logout } = usePrivy();
   const { wallets } = useWallets();
   const activeWallet = wallets[0];
+  console.log("activeWallet", activeWallet);
+  const isWrongNetwork = activeWallet?.chainId !== BASE_CHAIN_ID;
 
   return (
     <header
@@ -26,17 +30,24 @@ export function Header({ className }: HeaderProps) {
           <>
             <div className="flex items-center gap-2">
               {activeWallet?.address && (
-                <span className="text-sm text-muted-foreground">
-                  {activeWallet.address.slice(0, 6)}...
-                  {activeWallet.address.slice(-4)}
-                </span>
+                <>
+                  <span className="text-md text-muted-foreground">
+                    {activeWallet.address.slice(0, 10)}...
+                    {activeWallet.address.slice(-4)}
+                  </span>
+                  {isWrongNetwork ? (
+                    <span className="text-md text-red-500">Wrong Network</span>
+                  ) : (
+                    <span className="text-md text-green-500">Base</span>
+                  )}
+                </>
               )}
-              <Button variant="ghost" size="sm" onClick={logout}>
+              <Button variant="ghost" size="default" onClick={logout}>
                 Disconnect
               </Button>
             </div>
             <div className="h-8 w-8 rounded-full bg-primary/10 flex items-center justify-center border border-black">
-              <span className="text-sm font-medium">
+              <span className="text-md font-medium">
                 {user?.email?.address?.slice(0, 2).toUpperCase() || "AI"}
               </span>
             </div>
