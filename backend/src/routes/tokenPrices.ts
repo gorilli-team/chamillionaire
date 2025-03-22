2b186f6198c0db60acdf355f6799efc34c0b7a7c
Paolo Antonio Rossi <paolo@luduvigo.com>
2025-03-22 17:24:26 +0100

    backend: add Procfile and add last24h token prices endpoint

--- backend/src/routes/tokenPrices.ts
+++ backend/src/routes/tokenPrices.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { getTokenPrice } from "../services/PriceFeed";
+import { TokenPrice } from "../models/TokenPrice";
 const router = express.Router();
 
 // Get token price by symbol
-router.get("/:symbol", async (req, res) => {
+router.get("/token/:symbol", async (req, res) => {
   try {
     console.log(req.params.symbol);
     const tokenPrice = await getTokenPrice(req.params.symbol);
@@ -13,6 +14,17 @@ router.get("/:symbol", async (req, res) => {
     res.json(tokenPrice);
   } catch (error) {
     res.status(500).json({ message: "Error fetching token price", error });
+  }
+});
+
+router.get("/last24h", async (req, res) => {
+  try {
+    const tokenPrices = await TokenPrice.find({
+      createdAt: { $gte: new Date(Date.now() - 1000 * (60 * 60 * 24 - 1)) },
+    }).sort({ createdAt: -1 });
+    res.json(tokenPrices);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching token prices", error });
   }
 });
 
