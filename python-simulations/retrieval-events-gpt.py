82702032938cf5c6a0a285a511fc6fc68c24e144
Paolo Antonio Rossi <paolo@luduvigo.com>
2025-03-22 16:36:56 +0100

    python-simulations: start adding automation scripts

---
+++ python-simulations/retrieval-events-gpt.py
@@ -0,0 +1,139 @@
+import json
+import os
+from dotenv import find_dotenv, load_dotenv
+
+from openai import OpenAI
+from pydantic import BaseModel, Field
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=openai_api_key)
+
+"""
+docs: https://platform.openai.com/docs/guides/function-calling
+"""
+
+# --------------------------------------------------------------
+# Define the knowledge base retrieval tool
+# --------------------------------------------------------------
+
+
+def search_kb(question: str):
+    """
+    Loads all the available events from the JSON file.
+    """
+    with open("./events/new-listing.json", "r") as f:
+        return json.load(f)
+
+
+# --------------------------------------------------------------
+# Step 1: Call model with search_kb tool defined
+# --------------------------------------------------------------
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "search_kb",
+            "description": "Generate a trading signal based on the events in the knowledge base.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "question": {"type": "string"},
+                },
+                "required": ["question"],
+                "additionalProperties": False,
+            },
+            "strict": True,
+        },
+    }
+]
+
+system_prompt = "You are a helpful trading assistant that suggest the best operations on the market. You should always return a trading signal that you consider the best when the user asks."
+
+messages = [
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": "What trade should I do today?"},
+]
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=messages,
+    tools=tools,
+)
+
+# --------------------------------------------------------------
+# Step 2: Model decides to call function(s)
+# --------------------------------------------------------------
+
+completion.model_dump()
+
+# --------------------------------------------------------------
+# Step 3: Execute search_kb function
+# --------------------------------------------------------------
+
+
+def call_function(name, args):
+    if name == "search_kb":
+        return search_kb(**args)
+
+
+for tool_call in completion.choices[0].message.tool_calls:
+    name = tool_call.function.name
+    args = json.loads(tool_call.function.arguments)
+    messages.append(completion.choices[0].message)
+
+    result = call_function(name, args)
+    messages.append(
+        {"role": "tool", "tool_call_id": tool_call.id, "content": json.dumps(result)}
+    )
+
+# --------------------------------------------------------------
+# Step 4: Supply result and call model again
+# --------------------------------------------------------------
+
+
+class GeneratedSignal(BaseModel):
+    signal: str = Field(description="Either BUY or SELL.")
+    symbol: str = Field(description="The symbol of the token to buy or sell.")
+    quantity: float = Field(description="The quantity of the token to buy or sell.")
+    confidence_score: float = Field(description="The confidence you have that the signal will generate a positive return for the user.")
+    event_id: int = Field(description="The event id of the event chosen as the source for the generated signal")
+    motivation: str = Field(description="A brief explanation of the signal.")
+
+
+completion_2 = client.beta.chat.completions.parse(
+    model="gpt-4o",
+    messages=messages,
+    tools=tools,
+    response_format=GeneratedSignal,
+)
+
+# --------------------------------------------------------------
+# Step 5: Check model response
+# --------------------------------------------------------------
+
+final_response = completion_2.choices[0].message.parsed
+print(final_response)
+# final_response.answer
+# final_response.source
+
+# --------------------------------------------------------------
+# Question that doesn't trigger the tool
+# --------------------------------------------------------------
+
+# messages = [
+#     {"role": "system", "content": system_prompt},
+#     {"role": "user", "content": "What is the weather in Tokyo?"},
+# ]
+
+# completion_3 = client.beta.chat.completions.parse(
+#     model="gpt-4o",
+#     messages=messages,
+#     tools=tools,
+# )
+
+# completion_3.choices[0].message.content
